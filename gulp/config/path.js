9c0b59cf84bdd5de59e20c1bdbbe156822f6ba80
Azamat Namiyaly <azamatnamiyaly@gmail.com>
2023-04-27 17:07:43 +0600

    image optimization and converting to webp

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -10,19 +10,23 @@ export const path = {
     files: `${buildFolder}/files/`,
     html: `${buildFolder}/`,
     scss: `${buildFolder}/css/`,
-    js: `${buildFolder}/js/`
+    js: `${buildFolder}/js/`,
+    images: `${buildFolder}/img/`
   },
   src:{
     files: `${srcFolder}/files/**/*.*`,
     html: `${srcFolder}/*.html`,
     scss: `${srcFolder}/scss/style.scss`,
-    js: `${srcFolder}/js/app.js`
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`
   },
   watch: {
     files: `${srcFolder}/files/**/*.*`,
     html: `${srcFolder}/**/*.html`,
     scss: `${srcFolder}/scss/**/*.scss`,
-    js: `${srcFolder}/js/**/*.js`
+    js: `${srcFolder}/js/**/*.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg}`
   },
   clean: buildFolder,
   buildFolder: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -14,15 +14,17 @@ import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js"
+import { image } from "./gulp/tasks/image.js";
 
 const watcher = () => {
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.scss, js);
+  gulp.watch(path.watch.scss, image);
 }
 
-const mainTasks = gulp.parallel(html, copy, scss, js);
+const mainTasks = gulp.parallel(html, copy, scss, js, image);
 
 const dev =  gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
 
