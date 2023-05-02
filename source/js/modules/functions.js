951d076f5e3aeaf3d19a7d788da2d9a493e86112
Azamat Namiyaly <azamatnamiyaly@gmail.com>
2023-05-02 12:53:44 +0600

    add eslint and prettier config

--- source/js/modules/functions.js
+++ source/js/modules/functions.js
@@ -1,16 +1,21 @@
 export function isWebp() {
-  // Проверка поддержки webp
-  console.log('called')
-  function testWebP(callback) {
-    let webP = new Image();
-    webP.onload = webP.onerror = function () {
-      callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-  }
-  // Додавання класу _webp або _no-webp для HTML
-  testWebP(function (support) {
-    let className = support === true ? 'webp' : 'no-webp';
-    document.documentElement.classList.add(className);
-  });
+    function testWebP(callback) {
+        // eslint-disable-next-line
+        const webP = new Image();
+        function checkHeight() {
+            callback(webP.height === 2);
+        }
+
+        webP.onerror = checkHeight;
+        webP.onload = checkHeight;
+
+        webP.src =
+            'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    }
+    // Додавання класу _webp або _no-webp для HTML
+    testWebP((support) => {
+        const className = support === true ? 'webp' : 'no-webp';
+        // eslint-disable-next-line
+        window.document.documentElement.classList.add(className);
+    });
 }
